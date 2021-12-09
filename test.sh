1d04a058c0658a6e7afdb630b3c123f8a37fc382
Jan Richter <jrichter@redhat.com>
2021-12-09 10:17:48 +0100

    enable codecov uploader

--- test.sh
+++ test.sh
@@ -5,11 +5,10 @@ if [ ! -d node_modules ]; then
     npm install
 fi
 
-# Disabling code coverage for now because of CI issues.
-# curl -Os https://uploader.codecov.io/latest/linux/codecov
+curl -Os https://uploader.codecov.io/latest/linux/codecov
 
-# chmod +x codecov
+chmod +x codecov
 
 npm run verify
-# ./codecov --dir ./coverage
+./codecov --dir ./coverage
 
