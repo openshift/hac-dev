d2d5fd77ec00a5a829bf01542dc06b90a2e092a1
Andrew Ballantyne <aballant@redhat.com>
2021-12-16 12:56:43 -0500

    Adds temp dynamic-plugin-sdk k8s utils

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,16 @@ const { resolve } = require('path');
 const webpack = require('webpack');
 const fedModulePlugin = require('@redhat-cloud-services/frontend-components-config/federated-modules');
 
-const plugins = [fedModulePlugin({ root: resolve(__dirname, '../') })];
+const plugins = [
+  fedModulePlugin({
+    root: resolve(__dirname, '../'),
+    debug: true,
+    exposes: {
+      // Application root
+      './RootApp': resolve(__dirname, '../src/AppEntry'),
+    },
+  }),
+];
 
 // Save 20kb of bundle size in prod
 if (process.env.NODE_ENV === 'production') {
