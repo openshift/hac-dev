8d9503a94ec94d3fd26adb133304798921c65298
Andrew Ballantyne <8126518+andrewballantyne@users.noreply.github.com>
2021-12-20 11:34:55 -0500

    Adds temp dynamic-plugin-sdk k8s utils (#22)
    * Adds temp dynamic-plugin-sdk k8s utils
    
    * Improve the hook call to more benchmark like effort

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -4,17 +4,55 @@ const commonPlugins = require('./plugins');
 
 const environment = process.env.ENVIRONMENT || 'stage';
 const betaOrStable = process.env.BETA ? 'beta' : 'stable';
+// for accessing prod-beta change this to 'prod-beta'
 const env = `${environment}-${betaOrStable}`;
+
+const calculateRemoteConfig = (remoteConfig) => {
+  if (remoteConfig === 'stage') {
+    return 'https://console.stage.redhat.com';
+  } else if (remoteConfig === 'prod') {
+    return 'https://console.redhat.com';
+  }
+
+  return `https://${remoteConfig}.console.redhat.com`;
+};
 
 const webpackProxy = {
   deployment: process.env.BETA ? 'beta/apps' : 'apps',
   useProxy: true,
-  env, // for accessing prod-beta change this to 'prod-beta'
+  env,
   appUrl: process.env.BETA ? '/beta/hac/app-studio' : '/hac/app-studio',
   standalone: Boolean(process.env.STANDALONE),
+  ...(process.env.INSIGHTS_CHROME && {
+    localChrome: process.env.INSIGHTS_CHROME,
+  }),
+  customProxy: [
+    {
+      context: (path) => path.includes('/api/k8s'),
+      target:
+        'https://api-toolchain-host-operator.apps.appstudio-stage.x99m.p1.openshiftapps.com:443',
+      secure: false,
+      changeOrigin: true,
+      autoRewrite: true,
+      ws: true,
+      pathRewrite: { '^/api/k8s': '' },
+    },
+  ],
+  client: {
+    overlay: false,
+  },
   routes: {
+    // In order to serve your plugin locally change this line
+    '/api/plugins/console-demo-plugin': { host: 'http://localhost:9000' },
+    // '/beta/api/plugins/console-demo-plugin': { host: 'http://localhost:8003' },
+    // first part is the plugin URL, host is your localhost URL with port
     ...(process.env.API_PORT && {
       '/api/hac/app-studio': { host: `http://localhost:${process.env.API_PORT}` },
+    }),
+    ...(process.env.REMOTE_CONFIG && {
+      [`${process.env.BETA ? '/beta' : ''}/config`]: {
+        host: calculateRemoteConfig(process.env.REMOTE_CONFIG),
+      },
     }),
     ...(process.env.CONFIG_PORT && {
       [`${process.env.BETA ? '/beta' : ''}/config`]: {
