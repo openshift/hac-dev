bf3b52148c70d9ac828b6e6fd7f4c4ba38e0340e
Josef Kopriva <jkopriva@redhat.com>
2022-04-25 16:19:16 +0200

    Fixes after review
    Signed-off-by: Josef Kopriva <jkopriva@redhat.com>

--- integration-tests/support/commands/index.ts
+++ integration-tests/support/commands/index.ts
@@ -1,2 +1,7 @@
 // Include the cypress customized commands related files
 import './hooks';
+
+  //Handling errors from application
+  Cypress.on('uncaught:exception', (err) => {
+    return false;
+  })
--- integration-tests/support/pages/ApplicationComponentsPage.ts
+++ integration-tests/support/pages/ApplicationComponentsPage.ts
@@ -2,15 +2,8 @@ import { applicationComponentsPagePO } from '../pageObjects/createApplication-po
 import { alertTitle } from '../pageObjects/global-po';
 import { AbstractWizardPage } from './AbstractWizardPage';
 export class ApplicationComponentPage extends AbstractWizardPage {
-  //TODO
   createApplication() {
     cy.get(applicationComponentsPagePO.create).click();
-    cy.contains(applicationComponentsPagePO.createText).click();
-    cy.get('body').find(applicationComponentsPagePO.create).its('length').then(res=>{
-      if(res > 0){
-        cy.contains(applicationComponentsPagePO.createText).click();
-      }
-    });
   }
 
   checkAlert(message: string) {
