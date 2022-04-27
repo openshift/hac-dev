e72a9782e4c3df0c620034a4e346e42c86254da4
Mohammed Saud <msaud@redhat.com>
2022-04-27 17:18:33 +0530

    add error boundary to namespaced pages

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { ErrorBoundaryFallback } from './ErrorBoundaryFallback';
+
+export type ErrorBoundaryFallbackProps = {
+  errorMessage: string;
+  componentStack: string;
+  stack: string;
+  title: string;
+};
+
+type ErrorBoundaryProps = {
+  FallbackComponent?: React.ComponentType<ErrorBoundaryFallbackProps>;
+};
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+  error: Error;
+  errorInfo: React.ErrorInfo;
+};
+
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+      errorInfo: null,
+    };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    this.setState({
+      hasError: true,
+      error,
+      errorInfo,
+    });
+
+    // eslint-disable-next-line no-console
+    console.error('Caught error in a child component:', error, errorInfo);
+  }
+
+  render() {
+    const { hasError, error, errorInfo } = this.state;
+    const FallbackComponent = this.props.FallbackComponent || ErrorBoundaryFallback;
+
+    return hasError ? (
+      <FallbackComponent
+        title={error.name}
+        componentStack={errorInfo.componentStack}
+        errorMessage={error.message}
+        stack={error.stack}
+      />
+    ) : (
+      <>{this.props.children}</>
+    );
+  }
+}
