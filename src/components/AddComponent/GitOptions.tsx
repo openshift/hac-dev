5166ddec64a2481e67c45c023930cb57192dd86b
Rohit Rai <rohitkrai03@gmail.com>
2022-06-07 23:39:50 +0530

    Bring back multi component checkbox in git options

--- src/components/AddComponent/GitOptions.tsx
+++ src/components/AddComponent/GitOptions.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { ExpandableSection, Stack, StackItem } from '@patternfly/react-core';
-import { InputField } from '../../shared';
+import { CheckboxField, InputField } from '../../shared';
 
 export const GitOptions: React.FC = () => (
   <ExpandableSection isIndented toggleText="Git options">
@@ -19,6 +19,13 @@ export const GitOptions: React.FC = () => (
           label="Context dir"
           helpText="Optional subdirectory for the application source code."
           data-test="context-dir"
+        />
+      </StackItem>
+      <StackItem>
+        <CheckboxField
+          name="git.isMultiComponent"
+          label="Multi Component"
+          helpText="Whether the source contains multiple components."
         />
       </StackItem>
     </Stack>
