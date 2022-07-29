9e168a2c020c2b187ae9883eaa15ac006e317b21
mfrances <mfrances@redhat.com>
2022-07-29 15:32:23 -0400

    app getting started

--- src/hacbs/components/ApplicationDetails/__tests__/HacbsApplicationDetails.spec.tsx
+++ src/hacbs/components/ApplicationDetails/__tests__/HacbsApplicationDetails.spec.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import '@testing-library/jest-dom';
 import { useK8sWatchResource } from '@openshift/dynamic-plugin-sdk-utils';
 import { render, screen, configure } from '@testing-library/react';
+import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
 import { mockApplication } from '../../../../components/ApplicationDetailsView/__data__/mock-data';
-import HacbApplicationDetails from '../HacbsApplicationDetails';
+import { applicationQuickstartContent } from '../ApplicationQuickstartContent';
+import HacbsApplicationDetails from '../HacbsApplicationDetails';
 
 jest.mock('react-router-dom', () => ({
   Link: (props) => <a href={props.to}>{props.children}</a>,
@@ -14,21 +16,74 @@ jest.mock('@openshift/dynamic-plugin-sdk-utils', () => ({
   useK8sWatchResource: jest.fn(),
 }));
 
+jest.mock('@redhat-cloud-services/frontend-components/useChrome', () => ({
+  useChrome: jest.fn(),
+}));
+
+const useChromeMock = useChrome as jest.Mock;
+
 configure({ testIdAttribute: 'data-test' });
 
 const watchResourceMock = useK8sWatchResource as jest.Mock;
 
-describe('HacbApplicationDetails', () => {
+describe('HacbsApplicationDetails', () => {
   it('should render spinner if application data is not loaded', () => {
     watchResourceMock.mockReturnValue([[], false]);
-    render(<HacbApplicationDetails applicationName="test" />);
+    const set = jest.fn();
+    const toggle = jest.fn();
+    useChromeMock.mockReturnValue({
+      quickStarts: { set, toggle },
+    });
+    render(<HacbsApplicationDetails applicationName="test" />);
     expect(screen.queryByTestId('spinner')).toBeInTheDocument();
   });
 
   it('should render application display name if application data is loaded', () => {
     watchResourceMock.mockReturnValueOnce([mockApplication, true]);
-    render(<HacbApplicationDetails applicationName="test" />);
+    const set = jest.fn();
+    const toggle = jest.fn();
+    useChromeMock.mockReturnValue({
+      quickStarts: { set, toggle },
+    });
+    render(<HacbsApplicationDetails applicationName="test" />);
     expect(screen.queryByTestId('details__title')).toBeInTheDocument();
     expect(screen.queryByTestId('details__title').innerHTML).toBe('Test Application');
+  });
+
+  it('should only display quick start on first run', () => {
+    const gsApp = 'hacbs-getting-started-app';
+    const chromeStore = 'hac-dev';
+
+    localStorage.removeItem('hacbs/showApplicationQuickstart');
+    let showQuickStart = localStorage.getItem('hacbs/showApplicationQuickstart');
+    expect(showQuickStart).toBeNull();
+    watchResourceMock.mockReturnValueOnce([mockApplication, true]);
+    const set = jest.fn();
+    const toggle = jest.fn();
+    useChromeMock.mockReturnValue({
+      quickStarts: { set, toggle },
+    });
+
+    render(<HacbsApplicationDetails applicationName="test" />);
+    showQuickStart = localStorage.getItem('hacbs/showApplicationQuickstart');
+    // localStorage key doesn't exist, quickstart set/toggle should be called
+    expect(showQuickStart).toEqual('false');
+    expect(set).toHaveBeenCalledWith(chromeStore, [applicationQuickstartContent]);
+    expect(toggle).toHaveBeenCalledWith(gsApp);
+    set.mockClear();
+    toggle.mockClear();
+
+    render(<HacbsApplicationDetails applicationName="test" />);
+    // localStorage key exists, quickstart set/toggle should not be called
+    expect(set).not.toHaveBeenCalled();
+    expect(toggle).not.toHaveBeenCalled();
+    set.mockClear();
+    toggle.mockClear();
+
+    localStorage.removeItem('hacbs/showApplicationQuickstart');
+    render(<HacbsApplicationDetails applicationName="test" />);
+    // localStorage key removed, quickstart set/toggle should be called
+    expect(set).toHaveBeenCalledWith(chromeStore, [applicationQuickstartContent]);
+    expect(toggle).toHaveBeenCalledWith(gsApp);
   });
 });
