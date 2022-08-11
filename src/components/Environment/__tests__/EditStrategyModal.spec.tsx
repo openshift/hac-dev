21672bf72ef58a3499035de0411280c1fadc6dcd
Mohammed Saud <msaud@redhat.com>
2022-08-11 21:17:41 +0530

    add edit environment strategy modal

---
+++ src/components/Environment/__tests__/EditStrategyModal.spec.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import '@testing-library/jest-dom';
+import { k8sPatchResource } from '@openshift/dynamic-plugin-sdk-utils';
+import { act, render, screen } from '@testing-library/react';
+import { EnvironmentKind } from '../../../types';
+import { EditStrategyModal } from '../EditStrategyModal';
+
+jest.mock('@openshift/dynamic-plugin-sdk-utils', () => ({
+  k8sPatchResource: jest.fn(),
+}));
+
+const patchResourceMock = k8sPatchResource as jest.Mock;
+
+const devEnv: EnvironmentKind = {
+  kind: 'Environment',
+  apiVersion: 'appstudio.redhat.com/v1alpha1',
+  metadata: {
+    name: 'dev',
+    namespace: 'test',
+  },
+  spec: {
+    displayName: 'Dev',
+    deploymentStrategy: 'AppStudioAutomated',
+  },
+};
+
+const prodEnv: EnvironmentKind = {
+  kind: 'Environment',
+  apiVersion: 'appstudio.redhat.com/v1alpha1',
+  metadata: {
+    name: 'prod',
+    namespace: 'test',
+  },
+  spec: {
+    displayName: 'Prod',
+    deploymentStrategy: 'Manual',
+  },
+};
+
+describe('EditStrategyModal', () => {
+  it('should enable button if strategy is selected', async () => {
+    const { rerender } = render(<EditStrategyModal obj={devEnv} />);
+    const button = screen.getByRole('button', { name: 'Save' });
+    expect(button).toBeDisabled();
+    screen.getByRole('button', { name: 'Automatic' }).click();
+    screen.getByRole('menuitem', { name: 'Manual' }).click();
+    expect(button).toBeEnabled();
+
+    rerender(<EditStrategyModal obj={prodEnv} />);
+    expect(button).toBeDisabled();
+    screen.getByRole('button', { name: 'Manual' }).click();
+    screen.getByRole('menuitem', { name: 'Automatic' }).click();
+    expect(button).toBeDisabled();
+    await act(async () => screen.getByRole('checkbox').click());
+    expect(button).toBeEnabled();
+  });
+
+  it('updates resource when submitted', async () => {
+    patchResourceMock.mockResolvedValue({});
+    render(<EditStrategyModal obj={devEnv} onClose={() => {}} />);
+    screen.getByRole('button', { name: 'Automatic' }).click();
+    screen.getByRole('menuitem', { name: 'Manual' }).click();
+    await act(async () => screen.getByRole('button', { name: 'Save' }).click());
+    expect(patchResourceMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        queryOptions: { name: 'dev', ns: 'test' },
+        patches: [{ op: 'replace', path: '/spec/deploymentStrategy', value: 'Manual' }],
+      }),
+    );
+  });
+});
--- src/components/Environment/environment-actions.ts
+++ src/components/Environment/environment-actions.ts
@@ -3,10 +3,21 @@ import { Action } from '../../shared/components/action-menu/types';
 import { EnvironmentKind } from '../../types';
 import { createDeleteModalLauncher } from '../modal/DeleteResourceModal';
 import { useModalLauncher } from '../modal/ModalProvider';
+import { createEditStrategyModal } from './EditStrategyModal';
 
 export const useEnvironmentActions = (environment: EnvironmentKind): Action[] => {
   const showModal = useModalLauncher();
   return [
+    {
+      cta: () =>
+        showModal(
+          createEditStrategyModal({
+            obj: environment,
+          }),
+        ),
+      id: `edit-strategy-${environment.metadata.name.toLowerCase()}`,
+      label: 'Edit deployment strategy',
+    },
     {
       cta: () =>
         showModal(
