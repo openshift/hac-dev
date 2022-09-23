9a69e81348615217dee964f328f3a0e0fe0e5db6
Abhi <abhinandan13jan>
2022-09-23 17:42:54 +0530

    add pipelinerun & taskrun status

--- src/hacbs/components/TaskRunListView/__tests__/TaskRunListRow.spec.tsx
+++ src/hacbs/components/TaskRunListView/__tests__/TaskRunListRow.spec.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, configure } from '@testing-library/react';
 import { testTaskRun } from '../__data__/mock-TaskRun-data';
 import TaskRunListRow from '../TaskRunListRow';
+
+configure({ testIdAttribute: 'data-test' });
 
 describe('TaskRunListRow', () => {
   it('should render task info', () => {
@@ -9,9 +11,10 @@ describe('TaskRunListRow', () => {
       container: document.createElement('tr'),
     });
     const cells = wrapper.container.getElementsByTagName('td');
+    const status = wrapper.getAllByTestId('taskrun-status');
 
     expect(cells[0].innerHTML).toBe(testTaskRun.metadata.name);
     expect(cells[1].innerHTML).toBe(testTaskRun.spec.taskRef.name);
-    expect(cells[3].innerHTML).toBe('Failed');
+    expect(status[0].innerHTML).toBe('Failed');
   });
 });
