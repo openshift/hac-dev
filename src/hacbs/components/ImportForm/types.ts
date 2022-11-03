b2f64a06478c36a691962634f041425b15e5c9aa
karthik <karthik.jk11@gmail.com>
2022-11-03 10:13:46 +0530

    add optional label to mark a integration test as optional

--- src/hacbs/components/ImportForm/types.ts
+++ src/hacbs/components/ImportForm/types.ts
@@ -8,6 +8,14 @@ export type IntegrationTestFormValues = {
   optional: boolean;
 };
 
+export enum IntegrationTestAnnotations {
+  DISPLAY_NAME = 'app.kubernetes.io/display-name',
+}
+
+export enum IntegrationTestLabels {
+  OPTIONAL = 'test.appstudio.openshift.io/optional',
+}
+
 export type FormValues = ImportFormValues & {
   applicationData?: K8sResourceCommon;
   integrationTest: IntegrationTestFormValues;
