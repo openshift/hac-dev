27a00c865591d3ca5ef78ab6788c25b0d7aaa91e
Dheeraj Singh Jodha <dheerajsinghjodha@yahoo.com.au>
2022-11-10 20:51:25 +0530

    Increase the timeout for the text "1 of 1 merged"

--- integration-tests/support/pages/hacbs/CreateBuildPage.ts
+++ integration-tests/support/pages/hacbs/CreateBuildPage.ts
@@ -2,7 +2,7 @@ import { createBuildStepPO } from '../../pageObjects/hacbs-po';
 
 export class CreateBuildPage {
     confirmSuccessfulPRMerge() {
-        cy.get(createBuildStepPO.mergedCountHelperText).should('have.text', '1 of 1 merged');
+        cy.get(createBuildStepPO.mergedCountHelperText, { timeout: 120000 }).should('have.text', '1 of 1 merged');
         cy.get(createBuildStepPO.statusPRMerged).should('be.visible');
         cy.get(createBuildStepPO.mergePRButton).should('have.attr', 'aria-disabled', 'true');
     }
