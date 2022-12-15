d099cc194c015aef520b8c4c7d4421a39ef58122
karthik <karthik.jk11@gmail.com>
2022-12-15 19:44:44 +0530

    Update e2e tests to remove the integration step from import wizard flow

--- integration-tests/utils/HACBSApplications.ts
+++ integration-tests/utils/HACBSApplications.ts
@@ -1,42 +1,26 @@
-import { NavItem, pageTitles } from '../support/constants/PageTitle';
-import { actions } from '../support/pageObjects/global-po';
-import { CreateApplicationPage } from '../support/pages/CreateApplicationPage';
-import { Common } from './Common';
 import { applicationDetailPagePO } from '../support/pageObjects/createApplication-po';
 import {
-  actionsDropdown,
+  overviewTabPO,
   componentsTabPO,
   integrationTestsTabPO,
-  overviewTabPO
-} from '../support/pageObjects/pages-po';
-import { OverviewTabPage } from '../support/pages/tabs/OverviewTabPage';
-import { ComponentsTabPage } from '../support/pages/tabs/ComponentsTabPage';
+  actionsDropdown,
+} from '../support/pageObjects/hacbs-po';
 import { AddComponentPage } from '../support/pages/AddComponentPage';
 import { ComponentPage } from '../support/pages/ComponentsPage';
-import { CreateBuildPage } from '../support/pages/CreateBuildPage';
-import { AddIntegrationTestPage } from '../support/pages/AddIntegrationTestPage';
+import { AddIntegrationTestPage } from '../support/pages/hacbs/AddIntegrationTestPage';
+import { ComponentsTabPage } from '../support/pages/hacbs/tabs/ComponentsTabPage';
+import { CreateBuildPage } from '../support/pages/hacbs/CreateBuildPage';
+import { Applications } from './Applications';
+import { Common } from './Common';
+import { OverviewTabPage } from '../support/pages/hacbs/tabs/OverviewTabPage';
 
-export class Applications {
+export class HACBSApplications {
   static deleteApplication(applicationName: string) {
-    Common.navigateTo(NavItem.applications);
-    this.openKebabMenu(applicationName);
-    cy.get(actions.deleteItem).click();
-    cy.get(actions.deleteModalInput).clear().type(applicationName);
-    cy.get(actions.deleteModalButton).click();
-    cy.get(`[data-id="${applicationName}"]`).should('not.exist');
-  }
-
-  private static openKebabMenu(applicationName: string) {
-    cy.get(`[data-id="${applicationName}"]`).find(actions.kebabButton).click();
+    Applications.deleteApplication(applicationName);
   }
 
   static createApplication(name: string) {
-    const createApplicationPage = new CreateApplicationPage();
-    createApplicationPage.clickCreateApplication();
-    cy.testA11y(`${pageTitles.createApp} page`);
-    createApplicationPage.setApplicationName(name);
-    createApplicationPage.clickNext();
-    cy.testA11y(`Select source form`);
+    Applications.createApplication(name);
   }
 
   static createComponent(publicGitRepo: string, componentName: string) {
@@ -93,32 +77,29 @@ function addComponentStep(publicGitRepo: string) {
   addComponent.clickNext();
 }
 
-
 function reviewComponentsStep(componentName: string) {
   const componentPage = new ComponentPage();
 
   // Edit component name
-  componentPage.editComponentName(componentName + "-temp");
-  cy.contains('div', componentName + "-temp").should('be.visible');
+  componentPage.editComponentName(componentName + '-temp');
+  cy.contains('div', componentName + '-temp').should('be.visible');
 
   // Switch back to orginal name
   componentPage.editComponentName(componentName);
-  cy.contains('div', componentName).should('be.visible');;
+  cy.contains('div', componentName).should('be.visible');
 
   //Create Application
   componentPage.createApplication();
 }
 
-
 function createBuildStep() {
   new CreateBuildPage().clickNext();
 }
 
-
 export function addIntegrationTestStep(displayName: string, optionalForRelease: boolean = false) {
   const addIntegrationTestPage = new AddIntegrationTestPage();
-  const containerImage = 'quay.io/kpavic/test-bundle:pipeline'
-  const pipelineName = 'demo-pipeline'
+  const containerImage = 'quay.io/kpavic/test-bundle:pipeline';
+  const pipelineName = 'demo-pipeline';
 
   addIntegrationTestPage.enterDisplayName(displayName);
   addIntegrationTestPage.enterContainerImage(containerImage);
@@ -128,7 +109,9 @@ export function addIntegrationTestStep(displayName: string, optionalForRelease: 
     addIntegrationTestPage.markOptionalForRelease();
   }
 
-  cy.get('body').then(body => {
-    (body.find('button[type="submit"]').length > 0)? addIntegrationTestPage.clickNext() : addIntegrationTestPage.clickAdd();
+  cy.get('body').then((body) => {
+    body.find('button[type="submit"]').length > 0
+      ? addIntegrationTestPage.clickNext()
+      : addIntegrationTestPage.clickAdd();
   });
 }
