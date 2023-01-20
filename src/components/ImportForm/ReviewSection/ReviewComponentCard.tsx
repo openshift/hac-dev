956da7f39bba3f421642fb7cb8e4157de60d287c
karthik <karthik.jk11@gmail.com>
2023-01-20 10:37:34 +0530

    fix disappearing advanced options while switching runtime

--- src/components/ImportForm/ReviewSection/ReviewComponentCard.tsx
+++ src/components/ImportForm/ReviewSection/ReviewComponentCard.tsx
@@ -13,7 +13,6 @@ import {
   GridItem,
   TextInputTypes,
 } from '@patternfly/react-core';
-import { useFormikContext } from 'formik';
 import {
   EditableLabelField,
   EnvironmentField,
@@ -23,7 +22,7 @@ import {
   ResourceLimitField,
 } from '../../../shared';
 import ExternalLink from '../../../shared/components/links/ExternalLink';
-import { CPUUnits, DetectedFormComponent, ImportFormValues, MemoryUnits } from '../utils/types';
+import { CPUUnits, DetectedFormComponent, MemoryUnits } from '../utils/types';
 import { RuntimeSelector } from './RuntimeSelector';
 
 type ReviewComponentCardProps = {
@@ -46,9 +45,6 @@ export const ReviewComponentCard: React.FC<ReviewComponentCardProps> = ({
   const fieldPrefix = `components[${detectedComponentIndex}].componentStub`;
   const [expandedComponent, setExpandedComponent] = React.useState(isExpanded);
   const [expandedConfig, setExpandedConfig] = React.useState(true);
-  const {
-    values: { isDetected },
-  } = useFormikContext<ImportFormValues>();
 
   return (
     <Card
@@ -99,73 +95,71 @@ export const ReviewComponentCard: React.FC<ReviewComponentCardProps> = ({
         </CardTitle>
       </CardHeader>
       <CardExpandableContent>
-        {isDetected && (
-          <CardBody style={{ marginLeft: '2em', maxWidth: '70%' }}>
-            <ExpandableSection
-              isExpanded={expandedConfig}
-              onToggle={() => setExpandedConfig((v) => !v)}
-              toggleText="Deploy configuration"
-              isIndented
-            >
-              <FormSection>
-                <Grid hasGutter>
-                  <GridItem span={3}>
-                    <InputField
-                      name={`${fieldPrefix}.targetPort`}
-                      label="Target port"
-                      type={TextInputTypes.number}
-                    />
-                  </GridItem>
-                  <GridItem span={5} />
-                  <GridItem span={6}>
-                    <ResourceLimitField
-                      name={`${fieldPrefix}.resources.cpu`}
-                      unitName={`${fieldPrefix}.resources.cpuUnit`}
-                      label="CPU"
-                      minValue={0}
-                      unitOptions={CPUUnits}
-                      helpText="The amount of CPU the container is guaranteed"
-                    />
-                  </GridItem>
-                  <GridItem span={6}>
-                    <ResourceLimitField
-                      name={`${fieldPrefix}.resources.memory`}
-                      unitName={`${fieldPrefix}.resources.memoryUnit`}
-                      label="Memory"
-                      minValue={0}
-                      unitOptions={MemoryUnits}
-                      helpText="The amount of memory the container is guaranteed"
-                    />
-                  </GridItem>
-                </Grid>
-                <ExpandableSection toggleText="Show advanced deployment options">
-                  <FormSection>
-                    <NumberSpinnerField
-                      name={`${fieldPrefix}.replicas`}
-                      label="Instances"
-                      min={0}
-                      helpText="Number of instances of your image"
-                    />
-                    {/* <InputField
+        <CardBody style={{ marginLeft: '2em', maxWidth: '70%' }}>
+          <ExpandableSection
+            isExpanded={expandedConfig}
+            onToggle={() => setExpandedConfig((v) => !v)}
+            toggleText="Deploy configuration"
+            isIndented
+          >
+            <FormSection>
+              <Grid hasGutter>
+                <GridItem span={3}>
+                  <InputField
+                    name={`${fieldPrefix}.targetPort`}
+                    label="Target port"
+                    type={TextInputTypes.number}
+                  />
+                </GridItem>
+                <GridItem span={5} />
+                <GridItem span={6}>
+                  <ResourceLimitField
+                    name={`${fieldPrefix}.resources.cpu`}
+                    unitName={`${fieldPrefix}.resources.cpuUnit`}
+                    label="CPU"
+                    minValue={0}
+                    unitOptions={CPUUnits}
+                    helpText="The amount of CPU the container is guaranteed"
+                  />
+                </GridItem>
+                <GridItem span={6}>
+                  <ResourceLimitField
+                    name={`${fieldPrefix}.resources.memory`}
+                    unitName={`${fieldPrefix}.resources.memoryUnit`}
+                    label="Memory"
+                    minValue={0}
+                    unitOptions={MemoryUnits}
+                    helpText="The amount of memory the container is guaranteed"
+                  />
+                </GridItem>
+              </Grid>
+              <ExpandableSection toggleText="Show advanced deployment options">
+                <FormSection>
+                  <NumberSpinnerField
+                    name={`${fieldPrefix}.replicas`}
+                    label="Instances"
+                    min={0}
+                    helpText="Number of instances of your image"
+                  />
+                  {/* <InputField
                       name={`${fieldPrefix}.route`}
                       label="Route"
                       placeholder="Route exposed by the deployment"
                     /> */}
-                    <EnvironmentField
-                      name={`${fieldPrefix}.env`}
-                      envs={component.env}
-                      label="Environment variables"
-                      description="Component will have access during build and runtimes."
-                      labelIcon={
-                        <HelpTooltipIcon content="Set environment variables to define the behaviour of your application environment." />
-                      }
-                    />
-                  </FormSection>
-                </ExpandableSection>
-              </FormSection>
-            </ExpandableSection>
-          </CardBody>
-        )}
+                  <EnvironmentField
+                    name={`${fieldPrefix}.env`}
+                    envs={component.env}
+                    label="Environment variables"
+                    description="Component will have access during build and runtimes."
+                    labelIcon={
+                      <HelpTooltipIcon content="Set environment variables to define the behaviour of your application environment." />
+                    }
+                  />
+                </FormSection>
+              </ExpandableSection>
+            </FormSection>
+          </ExpandableSection>
+        </CardBody>
       </CardExpandableContent>
     </Card>
   );
