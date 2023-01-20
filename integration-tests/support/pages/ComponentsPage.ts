4a1fdeb0e2c010e0c4146c8831406e0b144db584
Rohit Rai <rohitkrai03@gmail.com>
2023-01-20 14:30:24 +0530

    Upgrade formik-pf to fix submit errors and fix e2e tests

--- integration-tests/support/pages/ComponentsPage.ts
+++ integration-tests/support/pages/ComponentsPage.ts
@@ -6,7 +6,7 @@ import { AbstractWizardPage } from './AbstractWizardPage';
 
 export class ComponentPage extends AbstractWizardPage {
   public componentName: string;
-  
+
   editComponentName(newName: string) {
     cy.get(ComponentsPagePO.editComponentNameIcon).eq(0).click();
     cy.get(ComponentsPagePO.editNameInput).clear().type(newName);
@@ -78,7 +78,7 @@ export class ComponentPage extends AbstractWizardPage {
   }
 
   createApplication() {
-    cy.get(ComponentsPagePO.create).trigger('click');
+    cy.get(ComponentsPagePO.create).click({ force: true });
     cy.get(ComponentsPagePO.create).should('be.disabled');
     Common.waitForLoad();
   }
