f7161974511931c686ed36dcf9a49aa0c9430350
Subhash Khileri <skhileri@redhat.com>
2023-02-01 18:46:19 +0530

    UI helper method update

--- integration-tests/utils/UIhelper.ts
+++ integration-tests/utils/UIhelper.ts
@@ -2,11 +2,11 @@
 export class UIhelper {
 
   static clickTab(tabName: string) {
-    cy.xpath(`//div[@data-test="app-details__tabs"]//button[child::span[text()='${tabName}']]`).click()
+    return cy.xpath(`//div[@data-ouia-component-type="PF4/Tabs"]//button[child::span[text()='${tabName}']]`).click()
   }
 
   static inputValueInTextBoxByLabelName(label: string, value: string) {
-    cy.xpath(`//div[@class="pf-c-form__group" and descendant::*[text()='${label}']]//input`).clear().type(value)
+    return cy.xpath(`//div[@class="pf-c-form__group" and descendant::*[text()='${label}']]//input`).clear().type(value)
   }
 
   static selectValueInDropdownbyLabelName(label: string, value: string) {
@@ -17,30 +17,30 @@ export class UIhelper {
   }
 
   static verifyLabelAndValue(label: string, value: string) {
-    cy.contains('div', label).contains('dd', value)
+    return cy.contains('div', label).contains('dd', value)
   }
 
   static clickButton(label: string, options?: { invoke?: boolean, force?: boolean }) {
     if (options?.invoke) {
-      cy.xpath(`//*[@data-ouia-component-type="PF4/Button" and text()='${label}']`).invoke("click");
+      return cy.xpath(`//*[@data-ouia-component-type="PF4/Button" and text()='${label}']`).invoke("click");
     }
     else if (options?.force) {
-      cy.xpath(`//*[@data-ouia-component-type="PF4/Button" and text()='${label}']`).click({ force: true });
+      return cy.xpath(`//*[@data-ouia-component-type="PF4/Button" and text()='${label}']`).click({ force: true });
     }
     else {
-      cy.xpath(`//*[@data-ouia-component-type="PF4/Button" and text()='${label}']`).click();
+      return cy.xpath(`//*[@data-ouia-component-type="PF4/Button" and text()='${label}']`).click();
     }
   }
 
   static clickLink(link: string, options?: { invoke?: boolean, force?: boolean }) {
     if (options?.invoke) {
-      cy.xpath(`//a[text()='${link}']`).invoke("click");
+      return cy.xpath(`//a[text()='${link}']`).invoke("click");
     }
     else if (options?.force) {
-      cy.xpath(`//a[text()='${link}']`).click({ force: true });
+      return cy.xpath(`//a[text()='${link}']`).click({ force: true });
     }
     else {
-      cy.xpath(`//a[text()='${link}']`).click();
+      return cy.xpath(`//a[text()='${link}']`).click();
     }
   }
 }
