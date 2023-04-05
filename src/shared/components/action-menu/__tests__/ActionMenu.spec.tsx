c091834886247c64fa1ecc183fc478b54c336abf
Mohammed Saud <msaud@redhat.com>
2023-04-05 20:30:54 +0530

    ensure outside menu click does not toggle state (#545)

--- src/shared/components/action-menu/__tests__/ActionMenu.spec.tsx
+++ src/shared/components/action-menu/__tests__/ActionMenu.spec.tsx
@@ -31,4 +31,60 @@ describe('Action Menu', () => {
       expect(screen.getByTestId('Action 2').textContent).toBe('Action 2');
     });
   });
+
+  it('should close the open menu when clicked on kebab menu', async () => {
+    render(
+      <BrowserRouter>
+        <ActionMenu
+          actions={[
+            {
+              cta: () => {},
+              id: 'action-1',
+              label: 'Action 1',
+            },
+            {
+              cta: () => {},
+              id: 'action-2',
+              label: 'Action 2',
+            },
+          ]}
+        />
+      </BrowserRouter>,
+    );
+    fireEvent.click(screen.getByTestId('kebab-button'));
+    fireEvent.click(screen.getByTestId('kebab-button'));
+
+    await waitFor(() => {
+      expect(screen.queryByText('Action 1')).toBeNull();
+      expect(screen.queryByText('Action 2')).toBeNull();
+    });
+  });
+
+  it('should close the open menu on clicking outside the menu', async () => {
+    render(
+      <BrowserRouter>
+        <ActionMenu
+          actions={[
+            {
+              cta: () => {},
+              id: 'action-1',
+              label: 'Action 1',
+            },
+            {
+              cta: () => {},
+              id: 'action-2',
+              label: 'Action 2',
+            },
+          ]}
+        />
+      </BrowserRouter>,
+    );
+    fireEvent.click(screen.getByTestId('kebab-button'));
+    fireEvent.click(document.body);
+
+    await waitFor(() => {
+      expect(screen.queryByText('Action 1')).toBeNull();
+      expect(screen.queryByText('Action 2')).toBeNull();
+    });
+  });
 });
