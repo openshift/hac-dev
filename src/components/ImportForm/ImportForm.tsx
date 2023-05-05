deb3b4d7234560916dae44eca7ff7ee8b7c1935a
Rohit Rai <rohitkrai03@gmail.com>
2023-05-05 13:15:23 +0530

    Update the title, description and help topics when in review mode

--- src/components/ImportForm/ImportForm.tsx
+++ src/components/ImportForm/ImportForm.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Bullseye, PageSection, Spinner } from '@patternfly/react-core';
+import { OpenDrawerRightIcon } from '@patternfly/react-icons/dist/esm/icons/open-drawer-right-icon';
+import { useApplicationBreadcrumbs } from '../../utils/breadcrumb-utils';
+import { HelpTopicLink } from '../HelpTopicLink/HelpTopicLink';
+import PageLayout from '../PageLayout/PageLayout';
 import GitImportForm from './GitImportForm';
 import SampleImportForm from './SampleImportForm';
 import { useValidApplicationName } from './utils/useValidApplicationName';
@@ -14,6 +18,14 @@ const ImportForm: React.FunctionComponent<ImportFormProps> = ({ applicationName 
   const [reviewMode, setReviewMode] = React.useState(false);
   const [validAppName, appNameloaded] = useValidApplicationName();
 
+  const applicationBreadcrumbs = useApplicationBreadcrumbs(applicationName);
+
+  const title = reviewMode ? 'Configure your components for deployment' : 'Grab some code';
+  const description = reviewMode
+    ? 'Review and define deployment settings and options.'
+    : 'Provide a link to your GitHub repository or start with a no-fail sample.';
+  const helpId = reviewMode ? 'rhtap-import-configure-component' : 'rhtap-import-add-component';
+
   if (!appNameloaded) {
     return (
       <Bullseye>
@@ -23,21 +35,34 @@ const ImportForm: React.FunctionComponent<ImportFormProps> = ({ applicationName 
   }
 
   return (
-    <PageSection isFilled className="import-form">
-      <GitImportForm
-        applicationName={applicationName}
-        recommendedApplicationName={validAppName}
-        reviewMode={reviewMode}
-        setReviewMode={setReviewMode}
-      />
-      <br />
-      {!reviewMode && (
-        <SampleImportForm
+    <PageLayout
+      breadcrumbs={[...applicationBreadcrumbs, { path: '#', name: 'Import' }]}
+      title={title}
+      description={
+        <>
+          {description}{' '}
+          <HelpTopicLink topicId={helpId} isInline>
+            Learn more <OpenDrawerRightIcon />
+          </HelpTopicLink>
+        </>
+      }
+    >
+      <PageSection isFilled className="import-form">
+        <GitImportForm
           applicationName={applicationName}
           recommendedApplicationName={validAppName}
+          reviewMode={reviewMode}
+          setReviewMode={setReviewMode}
         />
-      )}
-    </PageSection>
+        <br />
+        {!reviewMode && (
+          <SampleImportForm
+            applicationName={applicationName}
+            recommendedApplicationName={validAppName}
+          />
+        )}
+      </PageSection>
+    </PageLayout>
   );
 };
 
--- src/components/ImportForm/__tests__/ImportForm.spec.tsx
+++ src/components/ImportForm/__tests__/ImportForm.spec.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { render, screen, configure, waitFor } from '@testing-library/react';
+import { useApplicationBreadcrumbs } from '../../../utils/breadcrumb-utils';
 import ImportForm from '../ImportForm';
 import { useValidApplicationName } from '../utils/useValidApplicationName';
 import '@testing-library/jest-dom';
 
+jest.mock('@redhat-cloud-services/frontend-components/useChrome', () => ({
+  useChrome: () => ({
+    helpTopics: { setActiveTopic: jest.fn(), enableTopics: jest.fn(), disableTopics: jest.fn() },
+  }),
+}));
+
 jest.mock('../utils/useValidApplicationName', () => ({
   useValidApplicationName: jest.fn(),
+}));
+
+jest.mock('../../../utils/breadcrumb-utils', () => ({
+  useApplicationBreadcrumbs: jest.fn(),
 }));
 
 jest.mock('../GitImportForm', () => (props) => {
@@ -23,14 +34,42 @@ jest.mock('../SampleImportForm', () => () => {
 });
 
 const useValidApplicationNameMock = useValidApplicationName as jest.Mock;
+const useApplicationBreadcrumbsMock = useApplicationBreadcrumbs as jest.Mock;
 
 configure({ testIdAttribute: 'data-test' });
 
 describe('ImportForm', () => {
+  beforeEach(() => {
+    useApplicationBreadcrumbsMock.mockReturnValue([]);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should show the spinner when app name recommendation is not loaded', () => {
     useValidApplicationNameMock.mockReturnValue(['', false]);
     render(<ImportForm />);
     screen.getByRole('progressbar');
+  });
+
+  it('should render correct title and description for source section', () => {
+    useValidApplicationNameMock.mockReturnValue(['my-app-1', true]);
+    render(<ImportForm applicationName="my-app" />);
+    screen.getByText('Grab some code');
+    screen.getByText('Provide a link to your GitHub repository or start with a no-fail sample.');
+  });
+
+  it('should change the title and description for review mode', async () => {
+    useValidApplicationNameMock.mockReturnValue(['my-app-1', true]);
+    render(<ImportForm applicationName="my-app" />);
+    screen.getByText('Grab some code');
+    screen.getByText('Provide a link to your GitHub repository or start with a no-fail sample.');
+
+    await waitFor(() => screen.getByTestId('submit-button').click());
+
+    screen.getByText('Configure your components for deployment');
+    screen.getByText('Review and define deployment settings and options.');
   });
 
   it('should render the git and sample import forms', () => {
