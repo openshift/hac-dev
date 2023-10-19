32c885310e4d7fec3654c66222021fa8a712ee47
Mohammed Saud <msaud@redhat.com>
2023-10-19 21:20:36 +0530

    fix(access): use validate usernames in access form

--- src/components/UserAccess/UserAccessForm/__tests__/UsernameSection.spec.tsx
+++ src/components/UserAccess/UserAccessForm/__tests__/UsernameSection.spec.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import '@testing-library/jest-dom';
-import { fireEvent, screen } from '@testing-library/react';
+import { act, fireEvent, screen, waitFor } from '@testing-library/react';
 import { formikRenderer } from '../../../../utils/test-utils';
+import { validateUsername } from '../form-utils';
 import { UsernameSection } from '../UsernameSection';
+
+jest.mock('../form-utils', () => ({
+  validateUsername: jest.fn(),
+}));
+
+const validateMock = validateUsername as jest.Mock;
 
 describe('UsernameSection', () => {
   it('should show usernames field', () => {
@@ -12,16 +19,56 @@ describe('UsernameSection', () => {
     expect(screen.getByRole('searchbox')).toBeVisible();
   });
 
-  it('should add username chip when entered', () => {
+  it('should add username chip when entered', async () => {
+    validateMock.mockResolvedValue(true);
     formikRenderer(<UsernameSection />, { usernames: [] });
-    fireEvent.input(screen.getByRole('searchbox'), { target: { value: 'user1' } });
-    fireEvent.blur(screen.getByRole('searchbox'));
-    expect(screen.getByRole('list', { name: 'Chip group category' })).toBeVisible();
+    await act(async () => {
+      fireEvent.input(screen.getByRole('searchbox'), { target: { value: 'user1' } });
+    });
+    await waitFor(() =>
+      expect(screen.getByRole('list', { name: 'Chip group category' })).toBeVisible(),
+    );
     expect(screen.getByText('user1')).toBeVisible();
-    fireEvent.click(screen.getByRole('button', { name: 'Remove user1' }));
+
+    await act(async () => {
+      fireEvent.click(screen.getByRole('button', { name: 'Remove user1' }));
+    });
     expect(screen.queryByText('user1')).not.toBeInTheDocument();
-    fireEvent.input(screen.getByRole('searchbox'), { target: { value: 'user2' } });
-    fireEvent.blur(screen.getByRole('searchbox'));
+
+    validateMock.mockResolvedValue(true);
+    await act(async () => {
+      fireEvent.input(screen.getByRole('searchbox'), { target: { value: 'user2' } });
+    });
+    await waitFor(() => expect(screen.getByText('user2')).toBeVisible());
     expect(screen.getByText('user2')).toBeVisible();
+  });
+
+  it('should show correct field status while entering', async () => {
+    validateMock.mockResolvedValue(false);
+    formikRenderer(<UsernameSection />, { usernames: [] });
+    expect(
+      screen.getByText('Provide RHTAP usernames for the users you want to invite.'),
+    ).toBeVisible();
+    await act(async () => {
+      fireEvent.input(screen.getByRole('searchbox'), { target: { value: 'user!@#' } });
+    });
+    await waitFor(() => expect(screen.getByText('Invalid username format.')).toBeVisible());
+
+    await act(async () => {
+      fireEvent.input(screen.getByRole('searchbox'), { target: { value: 'user' } });
+    });
+    await waitFor(() => expect(screen.getByText('Validating...')).toBeVisible());
+    await waitFor(() => expect(screen.getByText('Username not found.')).toBeVisible());
+
+    validateMock.mockResolvedValue(true);
+    await act(async () => {
+      fireEvent.input(screen.getByRole('searchbox'), { target: { value: 'user1' } });
+    });
+    await waitFor(() => expect(screen.getByText('Validating...')).toBeVisible());
+    await waitFor(() => expect(screen.getByText('Validated')).toBeVisible());
+    await waitFor(() =>
+      expect(screen.getByRole('list', { name: 'Chip group category' })).toBeVisible(),
+    );
+    expect(screen.getByText('user1')).toBeVisible();
   });
 });
