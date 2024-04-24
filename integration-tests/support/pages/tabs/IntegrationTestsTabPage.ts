476efedea80e17d32f052603869aeda194a8280d
Sahil Budhwar <sahilbudhwar143@gmail.com>
2024-04-24 11:24:57 +0530

    fix(e2eTests): fix advance happy path and environment page tests

--- integration-tests/support/pages/tabs/IntegrationTestsTabPage.ts
+++ integration-tests/support/pages/tabs/IntegrationTestsTabPage.ts
@@ -30,7 +30,6 @@ export class IntegrationTestsTabPage {
     githubURL: string,
     revision: string,
     pathInRepository: string,
-    env: string,
     markOptionalForRelease?: string,
   ) {
     this.verifySaveChangesIsDisabled();
@@ -41,7 +40,6 @@ export class IntegrationTestsTabPage {
     }).should('be.visible');
     UIhelper.inputValueInTextBoxByLabelName('Revision', revision);
     UIhelper.inputValueInTextBoxByLabelName('Path in repository', pathInRepository);
-    UIhelper.selectValueInDropdownbyLabelName('Environment', env);
     if (markOptionalForRelease === 'uncheck') {
       cy.get(addIntegrationTestStepPO.optionalreleaseCheckbox).uncheck();
     } else if (markOptionalForRelease === 'check') {
