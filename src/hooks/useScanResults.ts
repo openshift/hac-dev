72cd8baa8414c49f256acd34d72fc73128b3c797
Abhi <abhinandan13jan@gmail.com>
2024-07-23 15:44:01 +0530

    feat(vulnerability): add new result type

--- src/hooks/useScanResults.ts
+++ src/hooks/useScanResults.ts
@@ -11,12 +11,14 @@ export const SCAN_RESULT = 'CLAIR_SCAN_RESULT';
 export const SCAN_RESULTS = 'CLAIR_SCAN_RESULTS';
 export const CVE_SCAN_RESULT = 'CVE_SCAN_RESULT';
 export const TEKTON_SCAN_RESULTS = 'TEKTON_SCAN_RESULTS';
+export const SCAN_OUTPUT = 'SCAN_OUTPUT';
 
 export const CVE_SCAN_RESULT_FIELDS = [
   SCAN_RESULT,
   SCAN_RESULTS,
   CVE_SCAN_RESULT,
   TEKTON_SCAN_RESULTS,
+  SCAN_OUTPUT,
 ];
 
 export const isCVEScanResult = (taskRunResults: TektonResultsRun) =>
