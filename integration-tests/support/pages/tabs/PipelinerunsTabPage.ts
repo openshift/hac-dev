c9944e945e6157e3b35eb4fd1ff3ad5597f25ca6
Katka92 <kkanova@redhat.com>
2024-10-22 11:46:29 +0200

    fix(e2e): fix and stabilize e2e tests

--- integration-tests/support/pages/tabs/PipelinerunsTabPage.ts
+++ integration-tests/support/pages/tabs/PipelinerunsTabPage.ts
@@ -185,7 +185,7 @@ export class TaskRunsTab {
       { name: `${pipelineName}-init`, task: 'init', status: 'Succeeded' },
       { name: `${pipelineName}-clone-repository`, task: 'clone-repository', status: 'Succeeded' },
       { name: `${pipelineName}-build-container`, task: 'build-container', status: 'Succeeded' },
-      { name: `${pipelineName}-show-summary`, task: 'show-summary', status: 'Succeeded' },
+      { name: `${pipelineName}-show-sbom`, task: 'show-sbom', status: 'Succeeded' },
     ];
   }
 
