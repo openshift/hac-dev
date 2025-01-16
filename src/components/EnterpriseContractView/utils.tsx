1242f2b60782b5e70363e2c3686dfda724317765
Sahil Budhwar <sahilbudhwar143@gmail.com>
2025-01-16 18:02:19 +0530

    fix: ec regex to extract voilation reports from tkn logs

--- src/components/EnterpriseContractView/utils.tsx
+++ src/components/EnterpriseContractView/utils.tsx
@@ -45,8 +45,26 @@ export const getRuleStatus = (type: ENTERPRISE_CONTRACT_STATUS) => {
   }
 };
 
+/**
+ * This regex expect the logs from tekton results to be in this formay
+ *
+ * ```
+ * step-vulnerabilities :-
+ * Lorem Ipsum some logs
+ *
+ * step-report-json :-
+ * {"success":true,"components":[{"name":"devfile-sample-code-with-quarkus-1",<... ec report in JSON ...>,}]}
+ *
+ * ```
+ *
+ */
+const EC_REPORT_JSON_REGEX = /((?<=step-report-json\s*:-\s*)(\{.*?\})(?=\s*step-|$))/g;
+
 export const extractEcResultsFromTaskRunLogs = (logs: string): EnterpriseContractResult => {
-  const extractedLogs = logs.match(/(\[report-json\] ).+/g);
-  const json = JSON.parse(extractedLogs.map((l) => l.replace('[report-json] ', '')).join(''));
-  return json;
+  const extractedLogs = logs.match(EC_REPORT_JSON_REGEX);
+  try {
+    return JSON.parse(extractedLogs[0]);
+  } catch {
+    return null;
+  }
 };
