e07cf1798a7c5908babee1b978b801082168058d
Sahil Budhwar <sahilbudhwar143@gmail.com>
2025-04-02 21:54:17 +0530

    fix: visit documentation link

--- src/components/AppBanner/AppBanner.tsx
+++ src/components/AppBanner/AppBanner.tsx
@@ -7,15 +7,16 @@ const AppBanner: React.FC<React.PropsWithChildren<unknown>> = () => {
   return (
     <div data-test="dev-preview-banner">
       <Banner variant="blue" className="app-banner" isSticky>
-        🎉 We’ve launched Konflux UI! The current version will be deprecated soon.{' '}
+        🎉 We’ve launched Konflux UI! The current version will be deprecated soon.
         <Button
           variant="link"
+          component="a"
           href="https://konflux.pages.redhat.com/docs/users/getting-started/ui-versions.html"
           target="_blank"
           isInline
         >
-          Visit the documentation
-        </Button>{' '}
+          {' Visit the documentation '}
+        </Button>
         to find the UI for your specific cluster.
       </Banner>
     </div>
